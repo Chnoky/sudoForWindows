4ba6973459bfa9a0f64284c59451eae9f7cf6796
Chnoky <80599198+Chnoky@users.noreply.github.com>
2021-03-14 00:12:29 +0100

    Create setting class to read settings file

---
+++ sudoWs_client/sudoWs_client/configuration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+public class Configuration
+{
+	private readonly String powershell;
+
+    public Configuration()
+    {
+        //String settingsFile = @"C:\Program Files\sudoWs\client\settings_client.cfg";
+        String settingsFile = @"C:\Workspace\sudoWs_client\sudoWs_client\settings_client.cfg";
+        String[] settings = File.ReadAllText(settingsFile).Split(@"\r\n");
+
+        foreach (String setting in settings)
+        {
+            String param = setting.Split("=")[0];
+            String value = setting.Split("=")[1];
+
+            if (param == "powershell")
+            {
+                this.powershell = value;
+            }
+        }
+    }
+
+	public String GetPowershell()
+	{
+		return this.powershell;
+	}
+
+}
