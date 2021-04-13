73c155c74082c620e965e4279129ac64847fcfd1
Chnoky <80599198+Chnoky@users.noreply.github.com>
2021-04-13 21:59:14 +0200

    Allow separators in sudoers for powershell arguments or options
    Sudoers line must match regex ^(u|g);(.*?);(.*);($|NOPASSWD$|PASSWD$)

--- sudoWs_server/sudoWs_server/getAutorization.cs
+++ sudoWs_server/sudoWs_server/getAutorization.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Text.RegularExpressions;
 
 public class GetAuthorization
 {
@@ -8,13 +9,15 @@ public class GetAuthorization
     {
 		string file = System.IO.File.ReadAllText(ConfigurationManager.AppSettings.Get("sudoers"));
 		string[] readPriviledges = file.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+		string sudoersPattern = @"^(u|g);(.*?);(.*);($|NOPASSWD$|PASSWD$)";
 
 		List<String[]> privilegesList = new List<String[]>();
 		for (int i = 0; i < readPriviledges.Length; i++)
 		{
-			if (readPriviledges[i].Split(";").Length >= 3 && readPriviledges[i].Split(";").Length <= 4)
+			if(Regex.IsMatch(readPriviledges[i], sudoersPattern))
 			{
-				privilegesList.Add(readPriviledges[i].Split(";"));
+				Match matchSyntax = Regex.Match(readPriviledges[i], sudoersPattern);
+				privilegesList.Add(new[] { matchSyntax.Groups[1].Value, matchSyntax.Groups[2].Value, matchSyntax.Groups[3].Value, matchSyntax.Groups[4].Value });
 			}
 		}
 
@@ -29,21 +32,25 @@ public class GetAuthorization
 		foreach(String[] priviledge in privileges)
         {
 			if(priviledge[0] == "u" && priviledge[1] == username)
-            {
+			{
 				if (priviledge[2] == commandLine)
-                {
+				{
 					if(priviledge[3] == "NOPASSWD")
-                    {
+					{
 						return 0;
 					}
-                    else
-                    {
+					else if(priviledge[3] == "PASSWD" || priviledge[3] == "") 
+					{
 						return 1;
 					}
-                }
-            }
-            else if(priviledge[0] == "g")
-            {
+					else
+					{
+						return 2;
+					}
+				}
+			}
+			else if(priviledge[0] == "g")
+			{
 				bool isAMember = GetUserInfo.IsUserGroupMember(username, priviledge[1]);
 
 				if (priviledge[2] == commandLine && isAMember)
